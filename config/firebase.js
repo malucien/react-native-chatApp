7ed9d58dfd84b7adb4e4f88db54c43b751edbe61
malucien <124107514+malucien@users.noreply.github.com>
2023-06-25 14:04:15 -0500

    Firebase configuration

---
+++ config/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  databaseURL: Constants.manifest.extra.databaseURL
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
